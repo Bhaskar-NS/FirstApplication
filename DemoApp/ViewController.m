fbcfe2acc19f82a92912f08370b56d8010308443
bhaskarns <test@tests-Mac.local>
2016-04-07 17:45:18 +0530

    initial commit

--- DemoApp/ViewController.m
+++ DemoApp/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *NameTxt;
+@property (strong, nonatomic) IBOutlet UILabel *NameLabel;
+@property (nonatomic,strong) NSString *Nme;
+
 
 @end
 
@@ -24,4 +28,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ButtonAction:(id)sender {
+    
+     self.Nme=self.NameTxt.text;
+    self.NameLabel.text=[NSString stringWithFormat:@"Hello %@",self.Nme];
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    self.Nme=self.NameTxt.text;
+    return YES;
+    
+}
 @end
